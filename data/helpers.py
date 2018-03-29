9ba732f1591eb7f47136c1efc74ad0cc1520f747
Thanos <thanosnokas@192.168.1.53>
2018-03-30 02:47:41 +0300

    Code cleanup

--- data/helpers.py
+++ data/helpers.py
@@ -30,8 +30,7 @@ def create_sequences(text, sequence_length, step):
         labels.append(text[i + sequence_length])
     return sequences, labels
 
-
-def create_model(sequence_length, vector_length):
+def create_model(sequence_length, chars_length):
     """
     Create the sequential model.
 
@@ -39,8 +38,8 @@ def create_model(sequence_length, vector_length):
     ----------
     sequence_length : int
         How long each sequence will be.
-    vector_length : int
-        How long each character verctor will be.
+    chars_length : int
+        How long each character vector will be.
     
     Returns
     -------
@@ -48,26 +47,12 @@ def create_model(sequence_length, vector_length):
         Keras sequential model.
     """
     model = Sequential()
-    model.add(LSTM(1024, input_shape=(sequence_length, vector_length)))
+    model.add(LSTM(1024, input_shape=(sequence_length, chars_length)))
     model.add(Dense(64))
-    model.add(Dense(vector_length))
+    model.add(Dense(chars_length))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='RMSprop')
     return model
-
-
-def sample(preds, temperature=1.0):
-
-    if temperature == 0:
-        temperature = 1
-
-    preds = np.asarray(preds).astype('float64')
-    preds = np.log(preds) / temperature
-    exp_preds = np.exp(preds)
-    preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
-    return np.argmax(probas)
-
 
 def get_unique_characters(text):
     """
@@ -84,11 +69,6 @@ def get_unique_characters(text):
         A list with all the unique characters.
     """
     return sorted(list(set(text)))
-
-
-def get_chars_index_dicts(chars):
-    return dict((c, i) for i, c in enumerate(chars)), dict((i, c) for i, c in enumerate(chars))
-
 
 def get_text(path):
     """
@@ -107,13 +87,53 @@ def get_text(path):
     with io.open(path, 'r', encoding='utf8') as f:
         return f.read().lower()
 
-def vectorize(sequences, sequence_length, chars, char_to_index, next_chars):
-    X = np.zeros((len(sequences), sequence_length, len(chars)), dtype=np.bool)
-    y = np.zeros((len(sequences), len(chars)), dtype=np.bool)
+def create_dictionaries(chars):
+    """
+    Returns two arrays as dictioanries, one as values/keys and one as keys/values.
+
+    Attributes
+    ----------
+    chars : list
+        List of unique characters.
+    
+    Returns
+    -------
+    list
+        List as values/keys.
+    list
+        List as keys/values.
+    """
+    return dict((c, i) for i, c in enumerate(chars)), dict((i, c) for i, c in enumerate(chars))
+
+def convert_to_one_hot(sequences, sequence_length, chars_length, char_to_index, labels):
+    """
+    Convert sequences and labels to one hot arrays.
+
+    Attributes
+    ----------
+    sequences : list
+        List that contains the sequences of the characters.
+    sequence_length : int
+        The length of each sequence.
+    chars_length : int
+        Length of the array of the unique characters.
+    char_to_index : list
+        A list that is a dictionary with the unique characters and indexes.
+    labels : list
+        List of labels.
+
+    Returns
+    -------
+    list
+        List of values as sequences of one hot arrays.
+    list
+        List of labels as one hot arrays.
+    """
+    x = np.zeros((len(sequences), sequence_length, chars_length), dtype=np.bool)
+    y = np.zeros((len(sequences), chars_length), dtype=np.bool)
     for i, sentence in enumerate(sequences):
         for t, char in enumerate(sentence):
-            X[i, t, char_to_index[char]] = 1
-        y[i, char_to_index[next_chars[i]]] = 1
+            x[i, t, char_to_index[char]] = 1
+        y[i, char_to_index[labels[i]]] = 1
 
-    return X, y
-
+    return x, y
--- data/main.py
+++ data/main.py
@@ -3,8 +3,6 @@ import helpers
 import numpy as np
 import random
 import sys
-from keras.preprocessing.text import Tokenizer
-from sklearn.preprocessing import OneHotEncoder
 
 # How many characters we look back.
 SEQUENCE_LENGTH = 80
@@ -19,52 +17,39 @@ EPOCHS = 8
 text = helpers.get_text(CORPUS)
 # Get unique characters.
 chars = helpers.get_unique_characters(text)
+# Get length of unique chars.
+chars_length = len(chars)
 
 # Create sequences that are the input values and the next characters that are the labels.
 values, labels = helpers.create_sequences(text, SEQUENCE_LENGTH, SEQUENCE_STEP)
 
-char_to_index, indices_char = helpers.get_chars_index_dicts(chars)
+char_to_index, index_to_char = helpers.create_dictionaries(chars)
 
-# Convert to vectors.
-X, y = helpers.vectorize(values, SEQUENCE_LENGTH, chars, char_to_index, labels)
-
-# tokenizer = Tokenizer(char_level=True, filters='')
-# tokenizer.fit_on_texts(text)
-# print(tokenizer.word_index)
-# tokenized = tokenizer.texts_to_sequences(values[-5:])
-# print(tokenized)
-# enc = OneHotEncoder()
-# enc.fit(tokenized)
-# oh = enc.transform(tokenized).toarray()
-# print(oh)
-# print(len(oh[0]))
-# exit()
+# Convert to one hot arrays.
+x, y = helpers.convert_to_one_hot(values, SEQUENCE_LENGTH, chars_length, char_to_index, labels)
 
 # Create model.
-model = helpers.create_model(SEQUENCE_LENGTH, len(chars))
+model = helpers.create_model(SEQUENCE_LENGTH, chars_length)
 
 # Train the model and save it to the disk.
-model.fit(X, y, batch_size=512, epochs=EPOCHS)
+model.fit(x, y, batch_size=512, epochs=EPOCHS)
 model.save_weights("model_weights.h5")
 # Uncomment the next line to use existing model weights. But you need to comment the two lines above.
 # You need to have already run it at least once to save the first model weights.
 # model.load_weights("model_weights.h5")
 
 # Create a first 80 chars seed.
-seed = u"Του Κίτσου η μάνα με τα τέσσερα πουλάκια\nκαι με τα τρια τουφέκια στο βράχο απάνω".lower()
+seed = u"Σύρε να ειπής της μάννας σου να μη σε καταρειέται\nνα δέσης το κεφάλι σου με δεκο".lower()
 
 for i in range(400):
-    x = np.zeros((1, SEQUENCE_LENGTH, len(chars)))
+    x = np.zeros((1, SEQUENCE_LENGTH, chars_length))
     for t, char in enumerate(seed):
         x[0, t, char_to_index[char]] = 1.
-
+    
     predictions = model.predict(x, verbose=0)[0]
-
     index = np.argmax(predictions)
-    # print(index)
 
-    next_char = indices_char[index]
-    # print(next_char)
+    next_char = index_to_char[index]
 
     seed = seed[1:] + next_char
 
