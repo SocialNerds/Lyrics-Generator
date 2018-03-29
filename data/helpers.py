e8d62c88b73deb41d6cfd7a787fb1ce8473f1247
Thanos <thanosnokas@192.168.1.53>
2018-03-29 22:38:28 +0300

    Trying to minimize code

--- data/helpers.py
+++ data/helpers.py
@@ -4,19 +4,53 @@ import io
 import numpy as np
 
 def create_sequences(text, sequence_length, step):
+    """
+    Create sequences that are the input values and the next characters that are the labels.
+
+    Attributes
+    ----------
+    text : string
+        The corpus that contains all the text.
+    sequence_length : int
+        How long each sequence will be.
+    step : int
+        How many steps each sequence will be apart.
+    
+    Returns
+    -------
+    list
+        List of sequences.
+    list
+        List of labels, that are the next characters.
+    """
     sequences = []
-    next_chars = []
+    labels = []
     for i in range(0, len(text) - sequence_length, step):
         sequences.append(text[i: i + sequence_length])
-        next_chars.append(text[i + sequence_length])
-    return sequences, next_chars
+        labels.append(text[i + sequence_length])
+    return sequences, labels
 
 
-def build_model(sequence_length, chars):
+def create_model(sequence_length, vector_length):
+    """
+    Create the sequential model.
+
+    Attributes
+    ----------
+    sequence_length : int
+        How long each sequence will be.
+    vector_length : int
+        How long each character verctor will be.
+    
+    Returns
+    -------
+    Sequetial
+        Keras sequential model.
+    """
     model = Sequential()
-    model.add(LSTM(1024, input_shape=(sequence_length, len(chars))))
+    model.add(LSTM(1024, input_shape=(sequence_length, vector_length)))
     model.add(Dense(64))
-    model.add(Dense(len(chars)))
+    model.add(Dense(vector_length))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='RMSprop')
     return model
@@ -35,7 +69,20 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 
-def extract_characters(text):
+def get_unique_characters(text):
+    """
+    Get text unique characters.
+
+    Attributes
+    ----------
+    text : string
+        The full text
+    
+    Returns
+    -------
+    list
+        A list with all the unique characters.
+    """
     return sorted(list(set(text)))
 
 
@@ -43,10 +90,22 @@ def get_chars_index_dicts(chars):
     return dict((c, i) for i, c in enumerate(chars)), dict((i, c) for i, c in enumerate(chars))
 
 
-def read_corpus(path):
+def get_text(path):
+    """
+    Get text of a file
+
+    Attributes
+    ----------
+    path : string
+        The file path
+    
+    Returns
+    -------
+    string
+        File text.
+    """
     with io.open(path, 'r', encoding='utf8') as f:
         return f.read().lower()
-
 
 def vectorize(sequences, sequence_length, chars, char_to_index, next_chars):
     X = np.zeros((len(sequences), sequence_length, len(chars)), dtype=np.bool)
--- data/main.py
+++ data/main.py
@@ -1,57 +1,65 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-import helper
+import helpers
 import numpy as np
 import random
 import sys
+from keras.preprocessing.text import Tokenizer
+from sklearn.preprocessing import OneHotEncoder
 
 # How many characters we look back.
 SEQUENCE_LENGTH = 80
 # On how many characters we split a sequence.
 SEQUENCE_STEP = 1
-# The file that contains the text
+# The file that contains the text.
 CORPUS = "corpus.txt"
 # How many epochs to train for.
-EPOCHS = 7
+EPOCHS = 3
 
 # Get the text from corpus.
-text = helper.read_corpus(CORPUS)
+text = helpers.get_text(CORPUS)
 # Get unique characters.
-chars = helper.extract_characters(text)
+chars = helpers.get_unique_characters(text)
 
-"""
-    Create sequences that will be used as the input to the network.
-    Create next_chars array that will serve as the labels during the training.
-"""
 # Create sequences that are the input values and the next characters that are the labels.
-values, labels = helper.create_sequences(text, SEQUENCE_LENGTH, SEQUENCE_STEP)
+values, labels = helpers.create_sequences(text, SEQUENCE_LENGTH, SEQUENCE_STEP)
 
-char_to_index, indices_char = helper.get_chars_index_dicts(chars)
+char_to_index, indices_char = helpers.get_chars_index_dicts(chars)
 
 # Convert to vectors.
-X, y = helper.vectorize(values, SEQUENCE_LENGTH, chars, char_to_index, labels)
+X, y = helpers.vectorize(values, SEQUENCE_LENGTH, chars, char_to_index, labels)
+
+# tokenizer = Tokenizer(char_level=True, filters='')
+# tokenizer.fit_on_texts(text)
+# print(tokenizer.word_index)
+# tokenized = tokenizer.texts_to_sequences(values[-5:])
+# print(tokenized)
+# enc = OneHotEncoder()
+# enc.fit(tokenized)
+# oh = enc.transform(tokenized).toarray()
+# print(oh)
+# print(len(oh[0]))
+# exit()
 
 # Create model.
-model = helper.build_model(SEQUENCE_LENGTH, chars)
+model = helpers.create_model(SEQUENCE_LENGTH, len(chars))
 
 # Train the model and save it to the disk.
-# model.fit(X, y, batch_size=512, epochs=EPOCHS)
-# model.save_weights("final.h5")
+model.fit(X, y, batch_size=512, epochs=EPOCHS)
+model.save_weights("model_weights.h5")
 # Uncomment the next line to use existing model weights. But you need to comment the two lines above.
 # You need to have already run it at least once to save the first model weights.
-model.load_weights("model_weights.h5")
+# model.load_weights("model_weights.h5")
 
 # Create a first 80 chars seed.
 seed = u"Του Κίτσου η μάνα με τα τέσσερα πουλάκια\nκαι με τα τρια τουφέκια στο βράχο απάνω".lower()
 
-for diversity in [0.2, 0.5, 1.0, 1.2]:
-    print()
+# diversities = [0.2, 0.5, 1.0, 1.2]
+diversities = [0.2]
+for diversity in diversities:
     print('----- diversity:', diversity)
 
     generated = ''
     generated += seed
-
-    print('----- Generating with seed: "' + seed + '"')
     sys.stdout.write(generated)
 
     for i in range(400):
@@ -60,11 +68,16 @@ for diversity in [0.2, 0.5, 1.0, 1.2]:
             x[0, t, char_to_index[char]] = 1.
 
         predictions = model.predict(x, verbose=0)[0]
-        next_index = helper.sample(predictions, diversity)
+    
+        # max_value = max(predictions)
+        # max_index = predictions.index(max_value)
+        print(np.argmax(predictions))
+
+        next_index = helpers.sample(predictions, diversity)
         next_char = indices_char[next_index]
 
         generated += next_char
-        sentence = sentence[1:] + next_char
+        seed = seed[1:] + next_char
 
         sys.stdout.write(next_char)
         sys.stdout.flush()
